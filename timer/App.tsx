9abc27d338e1d4a6e25268227e023265e2bc1583
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-08-23 17:55:45 -0500

    Center

--- timer/App.tsx
+++ timer/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <SafeAreaView style={style.contains}>
       <Text style={style.title}>Gestion de {''}</Text>
-      <Text>Horas</Text>
+      <Text style={style.title}>Horas</Text>
       <Button title="Iniciar Horas"></Button>
     </SafeAreaView>
   );
@@ -25,6 +25,7 @@ const style = StyleSheet.create({
   contains: {
     backgroundColor: '#F3F4f6',
     flex: 1,
+    alignItems: 'center',
   },
   title: {
     fontFamily: 'caption',
