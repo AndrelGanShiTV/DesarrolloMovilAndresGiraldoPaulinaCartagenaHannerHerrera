9197bd02053f96ebe754a51a95e1e7feb4c62dcd
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-08-23 18:40:28 -0500

    Press Botons

--- timer/App.tsx
+++ timer/App.tsx
@@ -9,6 +9,8 @@ import {
   useColorScheme,
   View,
   Button,
+  TextInput,
+  Pressable,
 } from 'react-native';
 
 const App = () => {
@@ -17,6 +19,21 @@ const App = () => {
       <Text style={style.title}>Gestion de {''}</Text>
       <Text style={style.title}>Horas</Text>
       <Button title="Iniciar Horas"></Button>
+
+      <Pressable
+        onPress={() => {
+          console.log('Presionaste El Boton');
+        }}>
+        <Text style={style.title}>Este es un boton</Text>
+      </Pressable>
+      {/* <View style={style.campo}>
+        <Text style={style.label}>Nombre Propietario</Text>
+        <TextInput
+          style={style.input}
+          placeholder="Nombre Propietario"
+          placeholderTextColor={'#ccc'}
+        />
+      </View> */}
     </SafeAreaView>
   );
 };
@@ -31,6 +48,23 @@ const style = StyleSheet.create({
     fontFamily: 'caption',
     fontSize: 25,
     color: '#5195FF',
+  },
+  label: {
+    color: '#5195FF',
+    marginBottom: 10,
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  campo: {
+    marginTop: 10,
+    marginHorizontal: 30,
+  },
+  input: {
+    backgroundColor: '#FFF',
+    color: '#000',
+    padding: 15,
+    borderRadius: 10,
   },
 });
 export default App;
