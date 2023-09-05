b2d5916078f2d539224a9cda4ee3ddbd2a7022d2
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-09-04 21:43:59 -0500

    IngresoHoras

--- timer/App.tsx
+++ timer/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Formulario from './src/components/Formulario';
+import IngresoHoras from './src/components/IngresoHoras';
 import {//elementos y componentes
   SafeAreaView,
   SafeAreaViewBase,
@@ -16,32 +17,41 @@ import {//elementos y componentes
 
 const App = () => {//array funcion
   const [modalVisible, setModalVisible] = useState(false)
+  const [modalIngresoHoras, setModalIngresoHoras] = useState(false)
 
   const cerrarModal = () => {
     setModalVisible(false)
+    setModalIngresoHoras(false)
   }
-
-
 
   return (
     //Solo se renderiza el elemento de mayor prioridad,en este caso el de mayor prioridad es safeareaview
     <SafeAreaView style={style.contains}>
 
       <Text style={style.title}>
-        GESTION DE
-        <Text>HORAS</Text>
+        GESTION DE HORAS
       </Text>
-
 
       <Pressable
         style={style.btnNuevaCita}
         onPress={() => setModalVisible(!modalVisible)}
       >
-        <Text style={style.btnNuevaCita}>crear cita</Text>
+        <Text style={style.btnTextoNuevaCita}>crear cita</Text>
       </Pressable>
       <Formulario
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
+      />
+
+      <Pressable
+        style={style.btnNuevaCita}
+        onPress={() => setModalIngresoHoras(!modalIngresoHoras)}
+      >
+        <Text style={style.btnTextoNuevaCita}>Registro Horas</Text>
+      </Pressable>
+      <IngresoHoras
+        modalIngresoHoras={modalIngresoHoras}
+        setModalIngresoHoras={setModalIngresoHoras}
       />
 
     </SafeAreaView>
@@ -62,33 +72,17 @@ const style = StyleSheet.create({
     color: '#5195FF',
     textAlign: 'center',
   },
-  label: {
-    color: '#5195FF',
-    marginBottom: 10,
-    marginTop: 15,
-    fontSize: 20,
-    fontWeight: '600',
-  },
-  campo: {
-    marginTop: 10,
-    marginHorizontal: 30,
-  },
-  input: {
-    backgroundColor: '#FFF',
-    color: '#000',
-    padding: 15,
-    borderRadius: 10,
-  },
   btnNuevaCita: {
     backgroundColor: '#6D28D9',
-    padding: 15,
+    padding: 10,
     borderRadius: 5,
+    margin: 10,
   },
   btnTextoNuevaCita: {
     textAlign: 'center',
     color: '#fff',
-    fontSize: 18,
-    fontWeight: '900',
+    fontSize: 20,
+    fontWeight: '500',
     textTransform: 'uppercase',
   },
 });
--- timer/src/components/IngresoHoras.js
+++ timer/src/components/IngresoHoras.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {
     //elementos y componentes
     SafeAreaView,
@@ -12,12 +12,105 @@ import {
 } from 'react-native';
 import DatePicker from 'react-native-date-picker'
 
-function IngresoHoras() {
+const Formulario = ({ modalIngresoHoras, setModalIngresoHoras }) => {
+    const [fecha, setFecha] = useState(new Date())
+    const [hora, setHora] = useState(new Date())
+
     return (
-        <div>
+        <Modal
+            animationType='slide'
+            visible={modalIngresoHoras}
+        >
+            <SafeAreaView style={style.contains}>
+                <ScrollView>
+                    <View>
+                        <Text style={style.title}>Registrar Horas</Text>
+                    </View>
+                    <View>
+                        <Text style={style.label}>Seleccione el Dia:</Text>
+                        <DatePicker
+                            locale='es'
+                            mode='date'
+                            date={fecha}
+                            onDateChange={() => setFecha(fecha)}
+                            style={style.date}
+                        />
+                    </View>
+                    <View>
+                        <Text style={style.label}>Seleccione la Hora:</Text>
+                        <DatePicker
+                            locale='es'
+                            mode='time'
+                            date={hora}
+                            onDateChange={() => setHora(fecha)}
+                            style={style.date}
+                        />
+                    </View>
+                    <View>
+                        <Pressable style={style.btnNuevaCita}
+                            onPress={() => { cerrarModal }}
+                        >
+                            <Text style={style.btnTextoNuevaCita}>
+                                Guardar
+                            </Text>
+                        </Pressable>
+                        <Pressable
+                            style={style.btnNuevaCita}
+                            onPress={() => setModalIngresoHoras(!modalIngresoHoras)}
+                        >
+                            <Text style={style.btnTextoNuevaCita}>
+                                Cancelar
+                            </Text>
+                        </Pressable>
+                    </View>
+                </ScrollView>
+            </SafeAreaView >
+        </Modal>
+    );
+};
 
-        </div>
-    )
-}
+const style = StyleSheet.create({
+    contains: {
+        backgroundColor: '#FFF',
+        flex: 1,
+        alignItems: 'center',
+    },
+    title: {
+        marginHorizontal: 20,
+        marginVertical: 20,
+        fontFamily: 'caption',
+        fontSize: 30,
+        color: '#5195FF',
+        textAlign: 'center',
+    },
+    label: {
+        color: '#5195FF',
+        marginBottom: 10,
+        marginTop: 15,
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    date: {
+        backgroundColor: '#6F3DF2',
+        color: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        marginHorizontal: 10,
+    },
+    btnNuevaCita: {
+        flex: 1,
+        backgroundColor: '#6D28D9',
+        padding: 15,
+        borderRadius: 5,
+        marginVertical: 5,
+    },
+    btnTextoNuevaCita: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: '900',
+        textTransform: 'uppercase',
+    },
+});
 
-export default IngresoHoras
+export default Formulario;
