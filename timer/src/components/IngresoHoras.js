702cae330a6a01ba177e48df3c5f1f607e56cf3a
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-09-04 20:24:45 -0500

    ads

---
+++ timer/src/components/IngresoHoras.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    //elementos y componentes
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Pressable,
+    Modal,
+    ScrollView,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker'
+
+function IngresoHoras() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default IngresoHoras
